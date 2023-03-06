278505dbf0196b38d906869721ee17c8d5a515fc
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2023-03-06 10:23:12 -0500

    final candidate - js/html/css updates

--- js/Dog.js
+++ js/Dog.js
@@ -1,29 +1,39 @@
-
+let likedTotal = 0;
 
 class Dog {
     constructor(data) {
         Object.assign(this, data)
     }
 
-    // Profile Viewed
-    profileFeeback() {
-        this.hasBeenSwiped = true;
-    }
-
     // Profile Loved
     profileLoved() {
         this.hasBeenLiked = true;
+        likedTotal += 1;
+    }    
+
+    // Profile Update
+    profileUpdate() {
+        this.hasBeenSwiped = true;
+        if(this.hasBeenLiked) {
+            document.getElementById('result').classList.add('profile-loved')
+        } else {
+            document.getElementById('result').classList.add('profile-disliked')
+        }
     }
 
-    // Create a do profile html/css
+    // Create a dog profile html/css
     getProfileHtml() {
-        const {name, avatar, age, bio, hasBeenSwiped, hasBeenLiked} = this; /*Deconstructing Object*/
+        /*Deconstructing Object*/
+        const {name, avatar, age, bio, hasBeenSwiped, hasBeenLiked} = this;
+        /*Set a bkg photo*/
         const profilePic = document.querySelector('.profile').style.backgroundImage = `url(${avatar})`;
+        /*html*/
         return `
+                <div class="profile-result-util hidden" id="result"></div>
                 <h1 class="user">${name}, ${age}</h1>
                 <h2 class="tag-line">${bio}</h2>
         `
     }
 }
 
-export default Dog
+export {Dog, likedTotal}
--- js/index.js
+++ js/index.js
@@ -1,37 +1,67 @@
 import dogsData from "./data.js";
-import Dog from "./dog.js";
+import {Dog, likedTotal} from "./dog.js";
 
 
 let profileArray = ["profile1", "profile2", "profile3"]
 
+function labelStatus() {
+    document.getElementById('result').classList.toggle('hidden')
+}
+
+// disable or enable buttons based on paramater
+function buttonStatus(par) {
+    const buttons = document.querySelectorAll('button');
+    for(let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = par;
+    }
+}
+
 function getNewProfile() {
     const nextProfile = dogsData[profileArray.shift()];
-    return nextProfile ? new Dog(nextProfile) : alert('No More Profiles')
+    return nextProfile ? new Dog(nextProfile) : {}
 }
 
 document.querySelector('.profile-select').addEventListener("click", (e) => {
     if(e.target.classList.contains("no-select")) {
-        console.log("Me No Like")
-        profileGallery.profileFeeback()
-        console.log(profileGallery)
-        profileGallery = getNewProfile()
-        render()
+        buttonStatus(true) /* Disables buttons based on parameter */
+        profileGallery.profileUpdate() /* Add Liked or Disliked Image */
+        labelStatus() /* Makes label image visible or hidden */
+        setTimeout( () => {
+            labelStatus()
+            profileGallery = getNewProfile()
+            buttonStatus(false)
+            render()    
+        }, 2000)
     } else if (e.target.classList.contains("love-select")) {
-        console.log("Me Like")
-        profileGallery.profileFeeback()
+        buttonStatus(true)
         profileGallery.profileLoved()
-        console.log(profileGallery)
-        profileGallery = getNewProfile()
-        render()
+        profileGallery.profileUpdate() /*Add Liked or Disliked Image*/
+        labelStatus()
+        setTimeout( () => {
+            if(profileArray.length > 0) {
+                labelStatus()
+                profileGallery = getNewProfile()
+                buttonStatus(false)
+                render()        
+            } else {
+                buttonStatus(true)
+                document.querySelector('.wrapper').innerHTML = `
+                <header>
+                    <div class="settings bkg-icon-util"></div>
+                    <div class="paw bkg-icon-util"></div>
+                    <div class="chat bkg-icon-util"></div>
+                </header>
+                <section>
+                    <h1>No more profile left 😄</h1>
+                    <h2>You liked a total of ${likedTotal} profiles 🐶.</h2>
+                </section>
+                `
+            }
+        }, 2000)
     }
 })
 
-
-// const chucho = new Dog(dogsData[0])
-
 function render() {
-    console.log(profileArray)
-    // console.log(document.querySelector('.profile').innerHTML = chucho.getProfileHtml())
     document.querySelector('.profile').innerHTML = profileGallery.getProfileHtml();
 }
 
